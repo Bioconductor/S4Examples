98c70214bfd57c8fb33c5c089af91969a33f42de
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-03-18 14:52:03 -0400

    expand longForm method

--- R/Example-methods.R
+++ R/Example-methods.R
@@ -9,9 +9,17 @@
 #'
 #' @returns A long form of the object
 #'
+#' @examples
+#' ex <- Example(list(a = 1, b = 2, c = 3))
+#' longForm(ex)
 #' @exportMethod longForm
 setMethod(
     "longForm", "Example", function(object, ...) {
-        print("longForm method for Example class")
+        ## lengths in object must be constant
+        all_lengths <- unique(lengths(object))
+        if (identical(length(all_lengths), 1L))
+            as.data.frame(object)[, c("group_name", "value")]
+        else
+            stop("All elements in object must have the same length")
     }
 )
