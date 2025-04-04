490139d3ee6620544626b5eed0980d99954b5558
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-04-04 17:05:24 -0400

    add isEmpty method

--- R/Example-methods.R
+++ R/Example-methods.R
@@ -4,7 +4,7 @@
 #'
 #' @importFrom BiocGenerics longForm
 #'
-#' @param object An object of class `Example`
+#' @param object,x An object of class `Example`
 #'
 #' @param ... Additional arguments
 #'
@@ -22,5 +22,24 @@ setMethod(
             as.data.frame(object)[, c("group_name", "value")]
         else
             stop("All elements in object must have the same length")
+    }
+)
+
+#' @name Example-methods
+#'
+#' @aliases isEmpty,Example-method
+#'
+#' @importFrom S4Vectors isEmpty
+#' @importFrom methods callNextMethod
+#'
+#' @examples
+#' isEmpty(
+#'     Example(x = list())
+#' )
+#'
+#' @exportMethod isEmpty
+setMethod(
+    "isEmpty", "Example", function(x) {
+        callNextMethod()
     }
 )
